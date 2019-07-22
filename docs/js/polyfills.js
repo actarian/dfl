3bb4b3bdba5fc810bd9c9560448424ff24254c38
actarian <lzampetti@gmail.com>
2019-07-22 15:35:23 +0200

    dfl

--- docs/js/polyfills.js
+++ docs/js/polyfills.js
@@ -287,6 +287,7 @@ if (!('Promise' in globalNS)) {
 
 // Polyfills for IE11
 
+/*
 if (typeof Object.assign != 'function') {
 	// Must be writable: true, enumerable: false, configurable: true
 	Object.defineProperty(Object, "assign", {
@@ -311,6 +312,38 @@ if (typeof Object.assign != 'function') {
 		},
 		writable: true,
 		configurable: true
+	});
+}
+*/
+
+if (!Object.assign) {
+	Object.defineProperty(Object, 'assign', {
+		enumerable: false,
+		configurable: true,
+		writable: true,
+		value: function(target, firstSource) {
+			'use strict';
+			if (target === undefined || target === null) {
+				throw new TypeError('Cannot convert first argument to object');
+			}
+			var to = Object(target);
+			for (var i = 1; i < arguments.length; i++) {
+				var nextSource = arguments[i];
+				if (nextSource === undefined || nextSource === null) {
+					continue;
+				}
+				nextSource = Object(nextSource);
+				var keysArray = Object.keys(Object(nextSource));
+				for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+					var nextKey = keysArray[nextIndex];
+					var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+					if (desc !== undefined && desc.enumerable) {
+						to[nextKey] = nextSource[nextKey];
+					}
+				}
+			}
+			return to;
+		}
 	});
 }
 
--- src/polyfills/polyfills.js
+++ src/polyfills/polyfills.js
@@ -1,5 +1,6 @@
 // Polyfills for IE11
 
+/*
 if (typeof Object.assign != 'function') {
 	// Must be writable: true, enumerable: false, configurable: true
 	Object.defineProperty(Object, "assign", {
@@ -24,6 +25,38 @@ if (typeof Object.assign != 'function') {
 		},
 		writable: true,
 		configurable: true
+	});
+}
+*/
+
+if (!Object.assign) {
+	Object.defineProperty(Object, 'assign', {
+		enumerable: false,
+		configurable: true,
+		writable: true,
+		value: function(target, firstSource) {
+			'use strict';
+			if (target === undefined || target === null) {
+				throw new TypeError('Cannot convert first argument to object');
+			}
+			var to = Object(target);
+			for (var i = 1; i < arguments.length; i++) {
+				var nextSource = arguments[i];
+				if (nextSource === undefined || nextSource === null) {
+					continue;
+				}
+				nextSource = Object(nextSource);
+				var keysArray = Object.keys(Object(nextSource));
+				for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+					var nextKey = keysArray[nextIndex];
+					var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+					if (desc !== undefined && desc.enumerable) {
+						to[nextKey] = nextSource[nextKey];
+					}
+				}
+			}
+			return to;
+		}
 	});
 }
 
